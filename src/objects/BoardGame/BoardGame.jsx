4502f16036612d96c43338a4158e657e3d7067da
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-02-20 19:03:47 -0300

    update in the UI

--- src/objects/BoardGame/BoardGame.jsx
+++ src/objects/BoardGame/BoardGame.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import CardGame from '../../components/CardGame/cardgame';
+import './BoardGame.css'
 
 const BoardGame = ({ amount }) => {
+    
     const arr = []
     for (let i = 0; i < amount; i++) {
         arr.push(1)
     }
     
-    return <>{arr.map(cards => <CardGame/>)}</>
+    return ( 
+    <div className='board-game'>
+        {arr.map(cards => <CardGame/>)}
+    </div> 
+    );
 }
  
 export default BoardGame;
