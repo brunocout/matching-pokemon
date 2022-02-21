c39bdadf877163108ccbe08c1172f9d03b9486e4
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-02-21 17:26:52 -0300

    Swap player when flip two cards : Introduce new feature

--- src/objects/BoardGame/BoardGame.jsx
+++ src/objects/BoardGame/BoardGame.jsx
@@ -3,10 +3,31 @@ import CardFrontBack from '../../components/CardFrontBack/CardFrontBack';
 import cards from './data';
 import './BoardGame.css'
 
-const BoardGame = ({ amount }) => {
+const BoardGame = () => {
+    const flipAndHideCards = (cards) => {
+        cards.forEach((card) => card.classList.remove('-active'))
+    }
+
+    const swapPlayer = () => {
+        const arrowDown = document.querySelector('.arrow-down')
+        const currentPlayer = arrowDown.getAttribute('data-currentplayer')
+        arrowDown.setAttribute('data-currentplayer', currentPlayer == 1 ? 2 : 1)
+    }
+
+    const handleOnClick = () => {
+        const boardGame = document.querySelector('.board-game')
+        const cardsActive = boardGame.querySelectorAll('.card-front-back.-active')
+
+        if (cardsActive.length == 2) {
+            setTimeout(() => {
+                flipAndHideCards(cardsActive)
+                swapPlayer()
+            }, 450);
+        }
+    }
     
     return ( 
-    <div className='board-game'>
+    <div className='board-game' onClick={handleOnClick}>
         {cards.map(card => <CardFrontBack icon={card.icon} alt={card.alt}/>)}
     </div> 
     );
