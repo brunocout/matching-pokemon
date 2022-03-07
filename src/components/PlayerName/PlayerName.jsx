2a69f79679d325062389c48ef52176bcd2c112f9
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-03-06 23:35:53 -0300

    adding dist

--- src/components/PlayerName/PlayerName.jsx
+++
@@ -1,7 +0,0 @@
-import './PlayerName.css'
-
-const PlayerName = ({ player }) => {
-    return <p className='player-name'>{player}</p>
-}
- 
-export default PlayerName;
--- src/components/VsPlayer/VsPlayer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './VsPlayer.css'
-
-const VsPlayer = ({ player = 'vs' }) => {
-    return <span className='vs-player'>{player}</span>
-}
- 
-export default VsPlayer;
