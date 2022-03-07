636d3ef8f5b35e8ff3b02ac42c71a501767a5c75
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-03-06 23:35:24 -0300

    Edit : edit styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,10 @@ import './Footer.css'
 const Footer = () => {
     return ( 
         <div className='footer'>
-            <img src="../assets/images/profile.png"/>
-            <p><a href="https://github.com/brunocout" target="_blank">@brunocout</a></p>
+            <p>
+                <img src="../assets/images/profile.png"/>
+                <a href="https://github.com/brunocout" target="_blank">@brunocout</a>
+            </p>
         </div>
      );
 }
